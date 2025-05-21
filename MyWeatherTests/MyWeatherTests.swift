cd577400eacc485242a1143f30dee1d9e69be349
shulgan <romashulgan@icloud.com>
2025-05-21 21:03:24 +0300

    Initial Commit

---
+++ MyWeatherTests/MyWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherTests.swift
+//  MyWeatherTests
+//
+//  Created by Роман Шульган on 24.04.2025.
+//
+
+import Testing
+@testable import MyWeather
+
+struct MyWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
